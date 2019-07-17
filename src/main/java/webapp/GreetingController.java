0eaf99881064c6d362edd6e848407b268fb55b22
Lampart_Thai_Huy_Binh <huybinh.lampart@gmail.com>
2019-07-17 13:17:51 +0700

    request method and paramUrl

--- src/main/java/webapp/GreetingController.java
+++ src/main/java/webapp/GreetingController.java
@@ -2,14 +2,23 @@ package webapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.GetMapping; //get only
+import org.springframework.web.bind.annotation.RequestMapping; //restful
+import org.springframework.web.bind.annotation.RequestMethod; //restful
+import org.springframework.web.bind.annotation.RequestParam; //request param
+import org.springframework.web.bind.annotation.PathVariable; //url variable
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    // @GetMapping("/greeting")
+    // public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    //     model.addAttribute("name", name);
+    //     return "greeting";
+    // }
+
+    @RequestMapping(value = {"/greeting","/greeting/{name}"}, method = RequestMethod.GET)
+    public String greeting(@PathVariable(required=false) String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
